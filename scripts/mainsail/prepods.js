aa6eec8c7239872fbb9cd6a622e9bf241964f8ff
Михаил Широков <shirokovmikhail2000@gmail.com>
2024-02-28 16:00:57 +0500

    prepods_0.1

--- scripts/mainsail/prepods.js
+++ scripts/mainsail/prepods.js
@@ -59,6 +59,7 @@ var prepodsPie	= new Highcharts.Chart({
     tooltip: tooltip,
     title: {
     	text: "",
+        padding: "left",
         
     },
     credits:
@@ -103,12 +104,12 @@ var prepodsPie	= new Highcharts.Chart({
                 
                 
             },
-            "size": "90%",
+            "size": "50%",
             "innerSize": "50%",
             "showInLegend": true,
             "animation": true,
             "borderWidth": 0,
-
+            // "center": ["50%", "30%"],
         }
     },
 
@@ -117,3 +118,64 @@ var prepodsPie	= new Highcharts.Chart({
         data: data
     }]
 });
+
+
+// Функция для загрузки Google таблицы и обработки данных
+function loadGoogleSheet() {
+    // ID вашей таблицы Google
+    var spreadsheetId = '1gwyyF2hdtwDjF4AG3xuaBW2O6Y0GXz0TvvLT5-kdfU4';
+    // ID листа с данными
+    var sheetId = '0';
+    // API ключ, полученный после создания проекта и включения Google Sheets API в Google Cloud Console
+    var apiKey = 'AIzaSyB8W0lONxmOxcPGzgrMCrbo3p2_OzrieaI';
+    // URL для запроса к Google Sheets API
+    var url = `https://sheets.googleapis.com/v4/spreadsheets/1gwyyF2hdtwDjF4AG3xuaBW2O6Y0GXz0TvvLT5-kdfU4/values/1!A1:E134?key=AIzaSyB8W0lONxmOxcPGzgrMCrbo3p2_OzrieaI`;
+
+  // Делаем AJAX запрос к API
+$.ajax({
+    url: url,
+    success: function(response) {
+        var values = response.values;
+        if (values && values.length > 0) {
+            // Начинаем с 1, чтобы пропустить заголовок таблицы
+            for (var i = 1; i < values.length; i++) {
+                var row = values[i];
+                
+                // Определяем переменные для фамилии, имени и отчества
+                var surname = row[0];
+                var name = row[1];
+                var patronymic = row[2];
+                
+                // Формируем ФИО в нужном формате
+                var fioFormatted = surname + ' ' + name.charAt(0) + '.' + patronymic.charAt(0) + '.';
+                
+                // Создаем строку таблицы и заполняем данными из таблицы Google
+                var tr = '<tr>';
+                tr += '<td>' + fioFormatted + '</td>'; // ФИО
+                tr += '<td>' + row[3] + '</td>'; // Должность
+                tr += '<td>' + row[4] + '</td>'; // Отдел
+                tr += '</tr>';
+                $('#perssonaleTable').append(tr);
+            }
+        }
+    }
+});
+ hideChartTrobbler();
+}
+
+// Запускаем функцию загрузки таблицы при загрузке страницы
+$(document).ready(function() {
+    loadGoogleSheet();
+});
+
+
+
+function hideChartTrobbler() {
+    // Получаем все элементы с id "charttrobber"
+    var chartTrobblerElements = document.querySelectorAll('#charttrobber');
+
+    // Проходимся по каждому элементу и скрываем его
+    chartTrobblerElements.forEach(function(element) {
+        element.style.display = 'none';
+    });
+}

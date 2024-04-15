334b14db836796e5a2680b5dccbb12f6351884ba
countgrey <mag.34737@gmail.com>
2024-04-15 12:00:02 +0500

    bimbimbambam

--- pages/loginscript.js
+++ pages/loginscript.js
@@ -6,7 +6,7 @@ loginForm.addEventListener("submit", async (event) => {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    const response = await fetch("http://127.0.0.1:5000/login", {
+    const response = await fetch("http://192.168.145.215:5000/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
